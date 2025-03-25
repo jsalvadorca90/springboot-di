a975fbc8eb799acefad4614d9d99753f2d4ecb50
Jose Castro <jsalvadorca90@gmail.com>
2025-03-24 22:25:09 -0500

    feat: Clase 59 a 61

--- src/main/java/com/udemy/springboot/di/app/springboot_di/repositories/ProductRepositoryImpl.java
+++ src/main/java/com/udemy/springboot/di/app/springboot_di/repositories/ProductRepositoryImpl.java
@@ -10,6 +10,10 @@ import com.udemy.springboot.di.app.springboot_di.models.Product;
 
 // se utiliza para indicar que un bean debe tener prioridad cuando se inyecta una dependencia y existen múltiples beans candidatos
 @Primary
+// // El scope pro defecto es 'singleton', el scope 'request' se utiliza para definir el alcance (scope) de un bean a nivel de petición HTTP. Esto significa que el contenedor de Spring creará una nueva instancia del bean cada vez que se reciba una nueva petición HTTP y esa instancia estará disponible solo durante el ciclo de vida de esa petición. Es especialmente útil en aplicaciones web donde la información de cada petición debe manejarse de forma independiente.
+// @RequestScope
+// // se utiliza para definir el alcance (scope) de un bean a nivel de sesión HTTP. Esto significa que el contenedor de Spring creará una nueva instancia del bean para cada sesión HTTP activa y esa instancia estará disponible durante toda la vida de esa sesión. Es una herramienta fundamental para construir aplicaciones web con estado de usuario.
+// @SessionScope
 // Esta anotación de Spring Framework indica que la clase ProductRepositoryImpl es un repositorio.En Spring,un repositorio es una clase que se encarga de acceder a la base de datos o a cualquier otra fuente de datos. Esta anotación es parte del módulo Spring Data y es especialmente útil cuando se trabaja con Spring Data JPA o Spring Data MongoDB, ya que facilita la creación de repositorios para interactuar con bases de datos.
 // "productList" especifica el nombre del bean para inyectar la dependencia del repositorio en otras clases utilizando la anotación @Autowired junto con @Qualifier("productList")
 @Repository("productList")
--- src/main/java/com/udemy/springboot/di/app/springboot_di/services/ProductServiceImpl.java
+++ src/main/java/com/udemy/springboot/di/app/springboot_di/services/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.udemy.springboot.di.app.springboot_di.models.Product;
@@ -14,39 +15,48 @@ import com.udemy.springboot.di.app.springboot_di.repositories.ProductRepository;
 // Implementación de la Interfaz ProductService
 public class ProductServiceImpl implements ProductService {
 
-    // anotación que permite inyectar objeto spring[bean/componente] obligatorio en atributo
+    // // inyectar dependencia mediante atributo
     // @Autowired
-    // si la inyección de dependencia es por atributo, el @Qualifier va después del @Autowired
+    // private Environment environment;
+    @Value("${config.tax.price}")
+    private Double tax;
+    // // anotación que permite inyectar objeto spring[bean/componente] obligatorio en atributo
+    // @Autowired
+    // // si la inyección de dependencia es por atributo, el @Qualifier va después del @Autowired
     // @Qualifier("productFoo")
     // Para el caso de @Primary: Si declaras 'ProductRepository productRepositoryFoo', como el nombre "productRepositoryFoo" es el mismo que el de la clase 'ProductRepositoryFoo' en camelCase, la elige por defecto sin usar @Primary, es como si tuviera el @Primary implicitamente
     private ProductRepository repository;
 
-    // @Autowired necesario en método 'set'
+    // // necesario en método 'set'
+    // @Autowired
     // public void setRepository(ProductRepository repository) {
     //     this.repository = repository;
     // }
 
+    // inyectar dependencia mediante constructor requiere "private Environment environment"
     // No es necesario colocar @Autowired cuando se trata de DI mediante constructor
-    // // @Qualifier le dice a Spring que inyecte el bean con el nombre "productRepositoryImpl", que corresponde a la clase "ProductRepositoryImpl"
-    public ProductServiceImpl(@Qualifier("productFoo") ProductRepository repository) {
+    // @Qualifier le dice a Spring que inyecte el bean con el nombre "productRepositoryImpl", que corresponde a la clase "ProductRepositoryImpl"
+    // public ProductServiceImpl(@Qualifier("productList") ProductRepository repository, Environment environment) {
+    public ProductServiceImpl(@Qualifier("productList") ProductRepository repository) {
         this.repository = repository;
+        // this.environment = environment;
     }
 
     @Override
     public List<Product> findAll() {
-        // findAll(): Se encarga de obtener todos los productos de una base de datos o
-        // de cualquier otra fuente de datos. .stream(): Convierte el resultado de
-        // repository.findAll() (que es una lista de productos) en un flujo (stream).
-        // .map(p -> { ... }): Este método map se aplica a cada elemento del flujo (cada
-        // producto). La parte p -> { ... } es una expresión lambda que define una
-        // función que se aplicará a cada producto.
+        // findAll(): Se encarga de obtener todos los productos de una base de datos o de cualquier otra fuente de datos. .stream(): Convierte el resultado de repository.findAll() (que es una lista de productos) en un flujo (stream).map(p -> { ... }): Este método map se aplica a cada elemento del flujo (cada producto). La parte p -> { ... } es una expresión lambda que define una función que se aplicará a cada producto.
         return repository.findAll().stream().map(p -> {
-            // Para cada producto p, se calcula un nuevo precio (priceTax) incrementando el
-            // precio original en un 25%.
-            Double priceTax = p.getPrice() * 1.25d;
-            // rompre PRINCIPIO DE INMUTABILIDAD
-            // // Se asigna el nuevo precio (convertido a un número entero largo) al
-            // atributo price del producto actual
+            // // muestra el valor de la propiedad "config.tax.price"
+            // System.out.println(environment.getProperty("config.tax.price", Double.class));
+            // // Para cada producto p, se calcula un nuevo precio (priceTax) incrementando el precio original en un 25%.
+            // // trabaja con la propiedad "config.tax.price" del archivo "config.properties"
+            // Double priceTax = p.getPrice() * environment.getProperty("config.tax.price", Double.class);
+            // // muestra el valor de la variable "tax"
+            // System.out.println(tax);
+            // trabaja con la variable "tax"
+            Double priceTax = p.getPrice() * tax;
+            // Romper PRINCIPIO DE INMUTABILIDAD
+            // // Se asigna el nuevo precio (convertido a un número entero largo) al atributo price del producto actual
             // p.setPrice(priceTax.longValue());
             // // Se devuelve el producto modificado con el nuevo precio
             // return p;
@@ -56,9 +66,11 @@ public class ProductServiceImpl implements ProductService {
             Product newProd = (Product) p.clone();
             newProd.setPrice(priceTax.longValue()); // se modifica el precio del producto clonado, no el original
             return newProd;
+            // // MUTABILIDAD de objeto 
+            // p.setPrice(priceTax.longValue()); // se modifica el precio del producto clonado, no el original
+            // return p;
 
-            // Finalmente, todos los productos modificados se recolectan en una nueva lista
-            // y se devuelve como resultado del método findAll()
+            // Finalmente, todos los productos modificados se recolectan en una nueva lista y se devuelve como resultado del método findAll()
         }).collect(Collectors.toList());
     }
 
